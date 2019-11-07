b125044f2c418736d3240c14b05ba6c08b6771ed
thecelestialjewel <christinemjordan6@gmail.com>
2019-11-07 12:13:18 -0800

    worked on data types, loops, methods, GUI, variables

--- Fortune/Form1.cs
+++ Fortune/Form1.cs
@@ -12,6 +12,13 @@ namespace Fortune
 {
     public partial class Form1 : Form
     {
+        private static Random random = new Random(DateTime.Now.Millisecond);
+
+        string[] FortuneCards = new string[23];
+        string[] FortuneMessages = new string[23]; //setting array up
+
+       
+
         public Form1()
         {
             InitializeComponent();
@@ -24,7 +31,30 @@ namespace Fortune
 
         private void Reveal_Click(object sender, EventArgs e)
         {
-
+            int[] luckynumbers = GenerateRandomLuckyNumbers();
+            string displayFortuneMessage = 
+                "Your fortune card is: " +
+                "\nYour reading means: " +
+                "\nYour lucky numbers are: " + string.Join(", ", luckynumbers);
+          //when you use \n it represents a new line \t will give you tab so if you want to indent the text
+            
+            
+            
+            MessageBox.Show(displayFortuneMessage, "Your fortune card");
         }
+
+        //https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/arrays/
+        public static int[] GenerateRandomLuckyNumbers()
+        {
+            int[] luckyNumbers = new int[6]; //[] sets amnt of numbers wanted for array
+            for (int i = 0; i < 6; i++)
+            {
+                luckyNumbers[i] = random.Next(1, 49);//
+            }
+            return luckyNumbers;
+        }
+
+        //TODO: private static void will be placing data into arrays, putting messages with the cards
+
     }
 }
